1ff825685813cebe5e8e9c40bd05987d5c0769a6
Daron Adkins <daron.adkins@gmail.com>
2018-06-27 15:44:03 -0500

    wip

--- public/themes/wordplate/front-page.php
+++ public/themes/wordplate/front-page.php
@@ -1,3 +1,12 @@
 <?php
 
-bladerunner('views.pages.front', ['socialLinks' => (new \KeriganSolutions\SocialMedia\SocialSettingsPage())->getSocialLinks()]);
+bladerunner('views.pages.front', [
+        'socialLinks' => (new \KeriganSolutions\SocialMedia\SocialSettingsPage())->getSocialLinks(),
+        'mainNav' => wp_nav_menu([
+                    'theme_location'  => 'main-navigation',
+                    'container_class' => 'collapse navbar-collapse',
+                    'menu_class'      => 'navbar-nav ml-auto',
+                    'echo'            => false
+                ])
+        ]
+);
--- public/themes/wordplate/views/layouts/main.blade.php
+++ public/themes/wordplate/views/layouts/main.blade.php
@@ -7,31 +7,9 @@
   {{ wp_head() }}
 </head>
 <body {{ body_class() }}>
-
     <div id="app">
-        <mobile-nav>
-            {{ wp_nav_menu([
-                'theme_location' => 'mobile-navigation',
-                'menu_class'     => 'navbar-nav m-auto',
-                'fallback_cb'    => 'main-navigation'
-            ]) }}
-        </mobile-nav>
         <header>
-            <nav role="navigation" class="navbar navbar-expand-md navbar-dark bg-dark">
-                <div class="container d-flex flex-column flex-md-row justify-content-between">
-                    <a class="navbar-brand" href="/">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mx-auto"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
-                    </a>
-                    <button @click="toggleMenu" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobilemenu" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    {{ wp_nav_menu([
-                        'theme_location'  => 'main-navigation',
-                        'container_class' => 'collapse navbar-collapse',
-                        'menu_class'      => 'navbar-nav ml-auto'
-                    ]) }}
-                </div>
-            </nav>
+            <main-navigation main-nav="{{ $mainNav }}"></main-navigation>
         </header>
 
         @yield('content')
