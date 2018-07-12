00b6dc396861d3ccda42d13c26e66e899a4fb326
Daron Adkins <daron.adkins@gmail.com>
2018-07-12 15:11:04 -0500

    fix projects front end module

--- public/themes/wordplate/front-page.php
+++ public/themes/wordplate/front-page.php
@@ -3,10 +3,8 @@
 bladerunner('views.pages.front', [
     'locations' => json_encode(get_terms([
         'taxonomy' => 'build-location',
-        'hide_empty' => false
     ])),
     'types' => json_encode(get_terms([
         'taxonomy' => 'construction-type',
-        'hide_empty' => false
     ]))
 ]);
