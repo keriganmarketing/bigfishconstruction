6142fa5d3247244e1e6474bbb6747f6be28ea8d7
Daron Adkins <daron.adkins@gmail.com>
2018-07-13 13:18:02 -0500

    add field functionality

--- public/themes/wordplate/functions.php
+++ public/themes/wordplate/functions.php
@@ -5,105 +5,22 @@ declare(strict_types=1);
 use KeriganSolutions\KMAPortfolio\Portfolio;
 use KeriganSolutions\KMATestimonials\Testimonial;
 use KeriganSolutions\KMATeam\Team;
+use KeriganSolutions\KMAContactInfo\ContactInfo;
 
 // Register plugin helpers.
 require template_path('includes/plugins/plate.php');
 
-$portfolio = (new Portfolio())->use();
-$testimonials = (new Testimonial())->menuIcon('editor-quote')->use();
-$team = (new Team())->use();
-
-/////////////////////////////////////////////////////////////
-if (function_exists('acf_add_options_page')) {
-    acf_add_options_page(array(
-        'page_title' => 'Contact Info',
-        'menu_title' => 'Contact Info',
-        'menu_slug' => 'contact-info',
-        'capability' => 'edit_posts',
-        'icon_url' => 'dashicons-info',
-        'redirect' => false
-    ));
-}
-acf_add_local_field_group(array(
-    'key' => 'group_contact_info',
-    'title' => 'Contact info',
-    'location' => array(
-        array(
-            array(
-                'param' => 'options_page',
-                'operator' => '==',
-                'value' => 'contact-info',
-            ),
-        ),
-    ),
-    'menu_order' => 0,
-    'position' => 'normal',
-    'style' => 'default',
-    'label_placement' => 'top',
-    'instruction_placement' => 'label',
-    'hide_on_screen' => '',
-));
-
-// title
-acf_add_local_field(array(
-    'key' => 'address',
-    'label' => 'Address',
-    'name' => 'address',
-    'type' => 'textarea',
-    'parent' => 'group_contact_info',
-));
-// email
-acf_add_local_field(array(
-    'key' => 'email',
-    'label' => 'Email',
-    'name' => 'email',
+(new Portfolio())->use();
+(new Testimonial())->menuIcon('editor-quote')->use();
+(new Team())->use();
+(new ContactInfo())->addField([
+    'key' => 'license_number',
+    'label' => 'License Number',
+    'name' => 'license_number',
     'type' => 'text',
     'parent' => 'group_contact_info',
-));
-// local phone
-acf_add_local_field(array(
-    'key' => 'phone',
-    'label' => 'Phone',
-    'name' => 'phone',
-    'type' => 'text',
-    'parent' => 'group_contact_info',
-));
+])->use();
 
-// toll-free phone
-acf_add_local_field(array(
-    'key' => 'toll_free_phone',
-    'label' => 'Toll Free Phone',
-    'name' => 'toll_free_phone',
-    'type' => 'text',
-    'parent' => 'group_contact_info',
-));
-// toll-free phone
-acf_add_local_field(array(
-    'key' => 'fax',
-    'label' => 'Fax',
-    'name' => 'fax',
-    'type' => 'text',
-    'parent' => 'group_contact_info',
-));
-// Image
-acf_add_local_field(array(
-    'key' => 'image',
-    'label' => 'Image',
-    'name' => 'image',
-    'type' => 'image',
-    'parent' => 'group_contact_info',
-    'instructions' => '',
-    'required' => 0,
-    'return_format' => 'array',
-    'preview_size' => 'medium',
-    'library' => 'all',
-    'min_width' => 0,
-    'min_height' => 0,
-    'max_width' => 0,
-    'max_height' => 0,
-));
-
-////////////////////////////////////////////////////////////
 
 /**
  * Registers the `build_location` taxonomy,
