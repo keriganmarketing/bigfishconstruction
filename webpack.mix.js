7afd296008aa53a0888576b32a2b6c83f80eb85a
programmer-man <bryan@kerigan.com>
2018-08-06 15:33:40 -0400

    google fonts

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,7 @@ mix.setPublicPath(`public/themes/${theme}/assets`);
 mix.js('resources/assets/scripts/app.js', 'scripts');
 mix.sass('resources/assets/styles/app.scss', 'styles');
 mix.styles([
+    'https://fonts.googleapis.com/css?family=Montserrat:400,700,800',
     'node_modules/alertifyjs/build/css/alertify.min.css',
     'node_modules/alertifyjs/build/css/themes/bootstrap.min.css',
     'public/themes/wordplate/assets/styles/app.css'
