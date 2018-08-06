1364c57314ff008fc528ffd7609f0392115908d7
programmer-man <bryan@kerigan.com>
2018-08-06 15:43:52 -0400

    wip

--- public/themes/wordplate/views/layouts/main.blade.php
+++ public/themes/wordplate/views/layouts/main.blade.php
@@ -4,6 +4,7 @@
   <meta charset="{{ bloginfo('charset') }}">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="theme-color" content="#6d9aea">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,800" rel="stylesheet">
   {{ wp_head() }}
 </head>
 <body {{ body_class() }}>
