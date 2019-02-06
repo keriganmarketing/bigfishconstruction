93c76fc4f1669604ba3a25ae51cdd322a392113c
Bryan Baird <bryan@kerigan.com>
2019-02-06 09:56:16 -0500

    temp hide sig

--- public/themes/wordplate/views/partials/footer.blade.php
+++ public/themes/wordplate/views/partials/footer.blade.php
@@ -30,7 +30,7 @@
     <p class="copyright text-center">State Certified Building Contractor – {{ get_field('license_number', 'option') }}</p>
     <p class="copyright text-center">&copy;{{ date('Y') }} {{ get_bloginfo() }}. All Rights&nbsp;Reserved. 
         <a style="text-decoration:underline;" href="/privacy-policy" >Privacy&nbsp;Policy</a> 
-        <span class="siteby">
+        {{-- <span class="siteby">
             <svg version="1.1" id="kma" xmlns="http://www.w3.org/2000/svg"
                 xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" height="14" width="20"
                 viewBox="0 0 12.5 8.7" style="enable-background:new 0 0 12.5 8.7;"
@@ -38,5 +38,6 @@
                     <path fill="#b4be35"
                 d="M6.4,0.1c0,0,0.1,0.3,0.2,0.9c1,3,3,5.6,5.7,7.2l-0.1,0.5c0,0-0.4-0.2-1-0.4C7.7,7,3.7,7,0.2,8.5L0.1,8.1 c2.8-1.5,4.8-4.2,5.7-7.2C6,0.4,6.1,0.1,6.1,0.1H6.4L6.4,0.1z"></path>
             </svg> &nbsp;<a href="https://keriganmarketing.com">Site&nbsp;by&nbsp;KMA</a>.
-        </span></p>
+        </span> --}}
+        </p>
 </footer>
